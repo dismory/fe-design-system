40a00964f31a78a85228c89eab3699dc74a7ca07
dismory <dismory@example.com>
2025-03-05 18:55:25 +0800

    feat: enable source map

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import pages from "vite-plugin-pages";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import pages from 'vite-plugin-pages';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     pages({
-      dirs: "src/pages",
-      extensions: ["jsx", "js", "tsx", "ts"],
+      dirs: 'src/pages',
+      extensions: ['jsx', 'js', 'tsx', 'ts'],
     }),
     tailwindcss(),
   ],
 
   build: {
-    outDir: "dist",
+    outDir: 'dist',
 
     emptyOutDir: true,
+    sourcemap: true,
   },
 });
