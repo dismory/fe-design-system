567b5f3eedfce8a7ecc415d5c13af8b627d50910
dismory <dismory@example.com>
2025-03-09 16:03:32 +0800

    update: modify demo page

--- src/pages/modalDialog.tsx
+++ src/pages/modalDialog.tsx
@@ -1,12 +1,16 @@
 import useTitle from "../hooks/useTitle";
 import ModalDialog from "../components/ModalDialog";
-import styles from "./modalDialog.module.css";
+import clsx from "clsx";
 
 function ModalDialogPage() {
   useTitle("ModalDialog Component");
 
   return (
-    <div className={styles.container}>
+    <div
+      className={clsx(
+        "flex h-screen w-screen items-center justify-center bg-neutral-900/70"
+      )}
+    >
       <ModalDialog
         title="Are you sure you want to leave the process?"
         description="Your upgrade plan process will be cancelled. You need to start again if you leave the process."
